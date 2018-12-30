8ece5119d739a0f0a9220f5486130ede5c5a3e7c
saint12maloj <saint12maloj@gmail.com>
2018-12-30 23:08:04 +0300

    Add project files.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 3, 7, 1, 20};
+            //array[0] = 3;
+            //array[1] = 7;
+            //array[2] = 1;
+            //array[3] = 20;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int t = array[i];
+                        array[i] = array[j];
+                        array[j] = t;
+
+                    }
+
+                }
+                Console.WriteLine(array [i]);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TryCatchFinally/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number_1 = 5;
+                int number_2 = 0;
+                int number_3 = number_1 / number_2; // деление на нуль ОШИБКА
+                Console.WriteLine(number_3); 
+            }
+            catch (Exception ex) // общий класс Exception с переменной ex
+            {
+                Console.WriteLine($"Возникла ошибка...{ex.Message}"); // обращение к "пропертям"
+            }
+            finally
+            {
+                Console.WriteLine("Приложение продолжает работу..."); // выводит в итоге продолжение
+            }
+            Console.ReadLine();
+        }
+    }
+}
